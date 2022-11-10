69bb3a7e3bb2b1fe296dafa9500effb48499615c
skiotto93 <fabio.forenza@hotmail.com>
2022-11-10 17:51:33 +0100

    Milestone 1 completata

--- main.js
+++ main.js
@@ -23,3 +23,41 @@ const images = [
         text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
     }
 ];
+/*--------------
+    CONFIG
+--------------*/
+//Milestone 1:
+// 1.a Ora rimuoviamo i contenuti statici e usiamo l’array di oggetti letterali per popolare dinamicamente il carosello.
+let currentImageIndex = 0;
+const currentImageContainer = document.querySelector('.view');
+const thumbsContainer = document.querySelector('.collection');
+
+/*--------------
+    MAIN
+--------------*/
+
+//Clono il template della pagina attraverso l'id
+const templateCurrentImage = document.getElementById('current-image').content.cloneNode(true);
+// Compilo l'html
+templateCurrentImage.querySelector('img').src = images[currentImageIndex].image;
+templateCurrentImage.querySelector('img').alt = images[currentImageIndex].title;
+templateCurrentImage.querySelector('.description h3').innerHTML = images[currentImageIndex].title;
+templateCurrentImage.querySelector('.description p').innerHTML = images[currentImageIndex].text;
+// Lo stampo in pagina
+currentImageContainer.append(templateCurrentImage);
+
+// THUMBS
+// Creo un ciclo con forEach
+images.forEach((element) => {
+    // Clono il template di thumbs
+    const templateThumbs = document.getElementById('thumbs').content.cloneNode(true);
+    // Compilo l'html
+    templateThumbs.querySelector('img').src = element.image;
+    templateThumbs.querySelector('img').alt = element.title;
+    // Lo stampo in pagina
+    thumbsContainer.append(templateThumbs);
+});
+// Al click dell'utente sulle frecce verso l'alto o il basso, l'immagine attiva diventerà visibile e dovremo aggiungervi titolo e testo.
+const start = document.querySelector('.up').addEventListener('click', function() {
+    document.querySelector('')
+})
